aa3ff370ef5b59a5b6193711defef1cbbbebba63
Cole Modelo <colemodelo@protonmail.com>
2021-08-13 03:48:48 -0600

    work section 3d cube prototype

--- assets/js/main.js
+++ assets/js/main.js
@@ -399,3 +399,20 @@
 					});
 
 })(jQuery);
+
+// 3D Cube Carosuel 
+
+let $carousel = $('.carousel'),
+    currentSlide, nextSlide;
+
+$('.next').click(function() {
+  currentSlide = $carousel.attr('data-slide');
+  nextSlide = +currentSlide === 4 ? 1 : +currentSlide + 1;
+  $carousel.attr('data-slide', nextSlide);
+});
+
+$('.prev').click(function() {
+  currentSlide = $carousel.attr('data-slide');
+  nextSlide = +currentSlide === 1 ? 4 : +currentSlide - 1;
+  $carousel.attr('data-slide', nextSlide);
+});
